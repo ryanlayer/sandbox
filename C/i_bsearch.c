434dfdcfae07c9c6679ea88d41967b007155ca2e
Ryan Layer <ryan.layer@gmail.com>
2011-05-31 22:23:19 -0400

    fix i bsearch

--- C/i_bsearch.c
+++ C/i_bsearch.c
@@ -2,21 +2,42 @@
 #include <stdlib.h>
 #include <math.h>
 
-int i_bsearch(int key, int *D, int D_size)
+int b_search(int key, int *D, int D_size, int lo, int hi)
 {
-	int lo = -1, hi = D_size, mid;
+	//int lo = -1, hi = D_size, mid;
+	int i = 0;
+	int mid;
 	while ( hi - lo > 1) {
+		++i;
 		mid = (hi + lo) / 2;
-		printf("%d,%d,%d\t", lo, mid, hi);
+		//printf("%d(%d)\t", mid, D[mid]);
 		if ( D[mid] < key )
 			lo = mid;
 		else
 			hi = mid;
 	}
 	
-	printf("\n");
+	printf("%d\n", i);
 
 	return hi;
+}
+
+int i_b_search(int key, int *D, int D_size, int *I, int I_size)
+{
+		int b = b_search(key, I, I_size, -1, I_size);
+
+		int lo = -1, hi = D_size;
+		int new_hi = ( (b+1)*hi + (I_size - (b+1))*lo ) / I_size;
+		int new_lo = ( (b)*hi + (I_size - (b+1))*lo ) / I_size;
+
+		if (b == 0)
+			new_lo = -1;
+		else if (b == I_size) {
+			new_hi = D_size;
+			new_lo = ( (b-1)*hi + (I_size - (b+1))*lo ) / I_size;
+		}
+
+		return b_search(key, D, D_size, new_lo, new_hi);
 }
 
 /*
@@ -58,21 +79,43 @@ int main(int argc, char *argv[])
 
 	qsort(D, d, sizeof(int), compare_int);
 
+	/*  Print list
 	for (j = 0; j < d; j++)
 		printf("%d %d\n", j, D[j]);
+	*/
 
+	int *I = (int *) malloc(i * sizeof(int));
 
 	for (j = 1; j < i; j++) {
 		int lo = -1, hi = d;
-		printf("%d %d\n",j, ( j*hi + (i - j)*lo ) / i);
+		I[ j - 1 ] = D[ ( j*hi + (i - j)*lo ) / i];
 	}
 
+	unsigned long bs = 0;
+	unsigned long is = 0;
+	int w = 0;
 
+	/* Search list */
 	for (j = 0; j < q; j++) {
+
+		printf("b:\n");
 		int r = rand();
-		printf("%d %d\n", r, i_bsearch(r, D, d));
+		start();
+		int a = b_search(r, D, d, -1, d);
+		stop();
+		bs += report();
+
+		printf("i:\n");
+		start();
+		int b = i_b_search(r, D, d, I, i);
+		stop();
+		is += report();
+
+		if (a != b)
+			++w;
 	}
 
+	printf("%lu\t%lu\t%f\n", bs, is, (((double)bs) / ((double)is)) );
 
 	return 0;
 }
